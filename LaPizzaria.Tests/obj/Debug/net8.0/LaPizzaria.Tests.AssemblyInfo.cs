86ab0d11e6f103234fac2bb494468c543494d93c
TrieuCum <ooishinimuran4@gmail.com>
2026-02-10 12:13:04 +0700

    update gd admin

--- LaPizzaria.Tests/obj/Debug/net8.0/LaPizzaria.Tests.AssemblyInfo.cs
+++ LaPizzaria.Tests/obj/Debug/net8.0/LaPizzaria.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaPizzaria.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57b48cd740415ad5a97f5f8ee5fb8d97ca33d05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6b180c70fb6c5e76c32b681b020e64bf73135c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaPizzaria.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaPizzaria.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
